a457644bae105d9105ba7978a8dbaf60e7814d17
Janet Huang <m934020033@gmail.com>
2021-01-02 00:52:25 -0800

    update

--- progress-step/index.js
+++ progress-step/index.js
@@ -1,7 +1,7 @@
 const next = document.getElementById('next')
 const prev = document.getElementById('prev')
 const progress = document.getElementById('progress')
-const circles = document.getElementsByClassName('circle')
+const circles = document.querySelectorAll('.circle')
 
 let currentActive = 1;
 next.addEventListener('click', ()=>{
@@ -11,8 +11,7 @@ next.addEventListener('click', ()=>{
         currentActive = circles.length 
     } 
 
-    circles.forEach((circle,idx) => {
-    console.log(circle) })
+    updateCircle()
     
 }
 )
@@ -24,13 +23,27 @@ prev.addEventListener('click',()=>{
     updateCircle()
 })
 
-// function updateCircle(){
-//     circles.forEach((circle,idx) => {
-//         console.log(circle)
-//         // if(idx < currentActive){
-//         //     circle.classList.add('active')
-//         // }else {
-//         //     circle.classList.remove('active')
-//         // }
-//     });
-// }
+function updateCircle() {
+    circles.forEach((circle,idx) => {
+        console.log(circle,idx)
+        if(idx < currentActive){
+            circle.classList.add('active')
+        }else {
+            circle.classList.remove('active')
+        }
+    });
+
+    let actives = document.querySelectorAll('.active')
+
+    progress.style.width = (actives.length-1)/(circles.length-1) *100 +'%'
+    console.log(progress)
+
+    if (currentActive===1){
+        prev.disabled = true;
+    }else if(currentActive ===4){
+        next.disabled = true
+    }else{
+        prev.disabled = false;
+        next.disabled = false;
+    }
+}
