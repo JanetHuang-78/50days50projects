e2a8d58562f2393dea83207962af51541e7ae0e1
Janet Huang <m934020033@gmail.com>
2021-01-12 22:43:38 -0800

    update

--- form-wave-animation/script.js
+++ form-wave-animation/script.js
@@ -1,8 +1,8 @@
 const labels = document.querySelectorAll('.form-control label')
 
 labels.forEach(label=>{
-    label.innerHTML = label.innerText
+    label.innerHTML = label.innerText/*抓取每一格字*/
     .split('')
-    .map((letter,idx)=>`<span style = "">${letter}</span>`)
+    .map((letter,idx)=>`<span style = "transition-delay:${idx*50}ms">${letter}</span>`)
     .join('')
 })
